6d7cd9dfb11b0e00cf929942af55f114156ffc06
Devisri-TK <tkdevisridev@gmail.com>
2024-05-07 15:14:03 -0400

    laravel-crud

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 8 CRUD Application - ItSolutionStuff.com</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+    <style>
+    .container {
+        margin-top: 50px;
+    }
+    .row {
+        margin: 25px 0px;
+    }
+    </style>
+</head>
+<body>
+  
+<div class="container">
+    @yield('content')
+</div>
+   
+</body>
+</html>
